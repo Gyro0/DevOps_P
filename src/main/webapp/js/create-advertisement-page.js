4149c762dba299feb2c5a46ac4c42bd57da20225
Gyro0 <oussayhabdou123@gmail.com>
2025-11-12 00:04:28 +0100

    fixed errors

--- src/main/webapp/js/create-advertisement-page.js
+++ src/main/webapp/js/create-advertisement-page.js
@@ -1,4 +1,3 @@
-
 /*
 Author: Matteo Piva
 
@@ -30,39 +29,58 @@ function createAdvertisement() {
     let idAdvertisement = 0;
     let title = document.getElementById("title").value;
     let description = document.getElementById("description").value;
-    let price = document.getElementById("price").value.toString();
-    let numTotItem = document.getElementById("numTotItem").value.toString();
-    let dateStart = document.getElementById("dateStart").value.toString();
-    let dateEnd = document.getElementById("dateEnd").value.toString();
-    let timeStart = document.getElementById("timeStart").value.toString();
-    let timeEnd = document.getElementById("timeEnd").value.toString();
-    let idType = document.getElementById("idType").value.toString();
+    let price = parseInt(document.getElementById("price").value);
+    let numTotItem = parseInt(document.getElementById("numTotItem").value);
+    let dateStart = document.getElementById("dateStart").value;
+    let dateEnd = document.getElementById("dateEnd").value;
+    let timeStart = document.getElementById("timeStart").value;
+    let timeEnd = document.getElementById("timeEnd").value;
+    let idType = parseInt(document.getElementById("idType").value);
 
     // Sanitize inputs
     title = sanitizeString(title);
     description = sanitizeString(description);
 
-    // Data to be send to the server
-    let data = "{\"advertisement\":" +
-        "{\"idAvdertisement\":\"" + idAdvertisement.toString() + "\"," +
-        "\"title\":\"" + title +
-        "\",\"description\":\"" + description + "\"," +
-        "\"price\":\"" + price + "\"," +
-        "\"score\":\"" + 0 + "\"," +
-        "\"numTotItem\":\"" + numTotItem + "\"," +
-        "\"dateStart\":\"" + dateStart + "\"," +
-        "\"dateEnd\":\"" + dateEnd + "\"," +
-        "\"timeStart\":\"" + timeStart + "\"," +
-        "\"timeEnd\":\"" + timeEnd + "\"," +
-        "\"emailCompany\":\"" + getUserEmail() + "\"," +
-        "\"idType\":\"" + idType + "\"}}";
+    // Ensure time fields are in HH:mm:ss format (required by backend)
+    if (!timeStart || timeStart === "") {
+        timeStart = "00:00:00";
+    } else if (timeStart.length === 5) {
+        timeStart = timeStart + ":00"; // Convert HH:mm to HH:mm:ss
+    }
+    
+    if (!timeEnd || timeEnd === "") {
+        timeEnd = "23:59:59";
+    } else if (timeEnd.length === 5) {
+        timeEnd = timeEnd + ":00"; // Convert HH:mm to HH:mm:ss
+    }
+
+    // Create JSON object properly
+    let advertisementData = {
+        advertisement: {
+            idAdvertisement: idAdvertisement,
+            title: title,
+            description: description,
+            price: price,
+            score: 0,
+            numTotItem: numTotItem,
+            dateStart: dateStart,
+            dateEnd: dateEnd,
+            timeStart: timeStart,
+            timeEnd: timeEnd,
+            emailCompany: getUserEmail(),
+            idType: idType
+        }
+    };
 
     $.ajax({
         type: "POST",
         url: url,
-        data: data,
+        data: JSON.stringify(advertisementData),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
+        headers: {
+            "Accept": "application/json"
+        },
         success: function(data){
 
             // Parses the JSON obj
@@ -73,10 +91,13 @@ function createAdvertisement() {
             window.location.href = contextPath + "/image-do-upload/" + idAdvertisement;
             },
         error: function(res) {
-            let resMessage = res.responseJSON.message;
-            alert(resMessage.message + " " + resMessage.errorDetails);
+            console.error("Error creating advertisement:", res);
+            if (res.responseJSON && res.responseJSON.message) {
+                let resMessage = res.responseJSON.message;
+                alert(resMessage.message + "\n" + (resMessage.errorDetails || ""));
+            } else {
+                alert("Failed to create advertisement. Status: " + res.status + "\nPlease check all fields and try again.");
+            }
         }
     });
-
-
 }
